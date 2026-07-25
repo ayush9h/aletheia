c774cbdbe8902a6a0b098213c534f88eb90ecc31
Ayush <ayush.kumar.21cse@bmu.edu.in>
2026-07-25 21:02:46 +0530

    fix: frontend build issue

--- client/src/app/hooks/useUserPref.ts
+++ client/src/app/hooks/useUserPref.ts
@@ -24,6 +24,7 @@ export function useUserPreferences(
             occupation: "",
             baseTone: "",
             userHobbies: "",
+            memoryEnabled:false,
           },
         });
       });
